2d576e60a5f9dd951705609bfe417fe308fb0503
Myslitel666 <tretyakov_02@bk.ru>
2026-08-29 11:14:02 +0500

    i

--- REQUESTS/SELECT.sql
+++ REQUESTS/SELECT.sql
@@ -3,3 +3,11 @@ SELECT *
 FROM UltraWords 
 WHERE DATE(DateTimeSaving) = '2026-08-25'
   AND strftime('%H', DateTimeSaving) = '19'; -- 19 час
+
+-- Записи дубли
+SELECT uw.Value, t.value, uw.Comment
+FROM Words w
+Join UltraWords uw
+  on lower(uw.Value) = w.value
+Join Types t
+  on uw.TypeId = t.id
