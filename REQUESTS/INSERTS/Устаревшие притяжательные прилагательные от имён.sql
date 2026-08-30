004fb2f0d5fae409015dd4bdb3f35fa74ed51b9e
Myslitel666 <tretyakov_02@bk.ru>
2026-08-30 16:51:59 +0500

    i

---
+++ REQUESTS/INSERTS/Устаревшие притяжательные прилагательные от имён.sql
@@ -0,0 +1,26 @@
+-- Обычные слова из файла АВП
+INSERT OR IGNORE INTO UltraWords (value, typeId, partOfSpeechId, isDeclinable, link, comment, DateTimeSaving, Popularity, IsModern)
+SELECT
+    value,
+    (SELECT Id FROM Types WHERE Value = 'Имена') AS typeId,
+    (SELECT Id FROM PartsOfSpeech WHERE Value = 'Прилагательное') AS partOfSpeechId,
+    1 AS isDeclinable,
+    'https://azbyka.ru/shemy/imena-biblejskie.shtml' AS link,
+    'Библия' AS comment,
+    datetime('now', 'localtime') AS DateTimeSaving,
+    0 AS Popularity,
+    0 AS IsModern
+FROM (
+    -- ============ СУЩЕСТВИТЕЛЬНЫЕ ============
+    SELECT 
+      OLD_NAMES_TRANSF(uw.value, c.id) as value, 
+      c.value
+    FROM UltraWords uw
+    JOIN Cases c
+      on c.id = 1
+    WHERE uw.comment = 'Библия'
+      AND SUBSTR(uw.value, -1) not in ('а','я','ь', 'ё')
+      AND uw.value NOT LIKE '% %'
+      AND SUBSTR(uw.value, -2) <> 'ов'
+    ORDER BY uw.value, c.id
+);
